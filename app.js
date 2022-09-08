19bceeafa127f01068539d43ff85a160c14c4194
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-09-08 12:00:04 +0800

    Learning the method getDay()

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.send("<h1>Yay it's the weekend!</h1>");
+    } else {
+        res.send("<h1>Boo! I have to work!</h1>");
+    }
+
+
 });
 
 
