df0d4f4ac8699b976d45d8db1b8b0c6b99785c6c
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-09-08 12:13:55 +0800

    res.sendFile

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.get("/", function(req, res) {
     if (currentDay === 6 || currentDay === 0) {
         res.write("<h1>Yay it's the weekend!</h1>");
     } else {
-        res.write("<p>It's not the weekend.</p>");
-        res.write("<h1>Boo! I have to work!</h1>");
-        res.send();
+        res.sendFile(__dirname + "/index.html");
     }
 
 
