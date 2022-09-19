0412d99bec28f61df92c6e07a16e949603242a82
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-09-20 01:28:43 +0800

    code optimization in date.js and app.js files

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     
-    let day = date.getDay();
+    const day = date.getDate();
     
     res.render("list", {listTitle: day, newListItems: items});
 
@@ -24,7 +24,7 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
 
